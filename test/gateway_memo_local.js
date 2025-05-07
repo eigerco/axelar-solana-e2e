867d40d253edd8c4c34c4f40baa48c6fc72c484e
ICavlek <ivan.cavlek1403@gmail.com>
2025-05-07 19:17:08 +0200

    chore: small mods

--- test/gateway_memo_local.js
+++ test/gateway_memo_local.js
@@ -16,9 +16,7 @@ const EvmChain = "avalanche-fuji";
 
 const SolanaChain = "localnet";
 const SolanaRpc = "http://127.0.0.1:8899";
-const SolanaKey = "[204,155,157,121,82,50,222,240,73,148,148,33,230,17,186,159,28,168,41,233,236,211,115,247,167,188,69,126,153,80,214,126,13,74,42,170,113,206,235,3,60,84,234,14,188,183,7,103,74,226,232,177,114,40,62,21,157,40,88,249,104,36,211,8]";
-
-const MemoAddress = "mem7LhKWbKydCPk1TwNzeCvVSpoVx2mqxNuvjGgWAbG";
+const SolanaKey = "<insert_private_key_from_~/.config/solana/id.json>";
 
 function setupConnections() {
     const solanaConnection = new solanaWeb3.Connection(SolanaRpc, {
@@ -59,7 +57,7 @@ describe("Gateway Memo Interaction", function() {
 
     before(async () => {
         solanaMemoProgram = axelarSolanaMemoProgramProgram({
-            programId: new PublicKey(MemoAddress),
+            programId: new PublicKey(AXELAR_SOLANA_MEMO_PROGRAM_PROGRAM_ID),
             provider: setup.solana.provider,
         });
     });
